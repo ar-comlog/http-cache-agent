d546e2294f9394f8f098444ee876328af5c5518d
ar <a.rau@comlog.org>
2022-04-07 11:29:01 +0200

    ~ optimizing

---
+++ dist/types.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="node" />
+import { Agent, AgentOptions, ClientRequest } from "agent-base";
+import tls from "tls";
+export interface Header {
+    protocol?: string;
+    statusCode?: string;
+    statusMessage?: string;
+    expires?: string;
+}
+export interface CAClientRequest extends ClientRequest {
+    path: string;
+    pathname: string;
+}
+export interface CAOptions extends AgentOptions, tls.ConnectionOptions {
+    filepath?: string;
+    prefix?: string;
+    secureEndpoint?: boolean;
+    agent?: Agent;
+}
